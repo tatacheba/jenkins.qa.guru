13219cf7b2ad1d3105fc51dd59b5bf7fd03da8bd
TatianaCheb <tanya.cheba12@gmail.com>
2022-03-12 16:50:41 +0300

    Update Les10TestFormJenkins.java

--- src/test/java/Les10TestFormJenkins.java
+++ src/test/java/Les10TestFormJenkins.java
@@ -3,36 +3,16 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import pages.RegistrationPages;
-import pages.components.CalendarComponent;
-import pages.components.PathComponent;
 
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Configuration.browserSize;
-import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static io.qameta.allure.Allure.step;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
 
 public class Les10TestFormJenkins {
-
-    RegistrationPages registrationPage = new RegistrationPages();
-    CalendarComponent calendarComponent = new CalendarComponent();
-    PathComponent pathComponent = new PathComponent();
-    String
-            openPagesUrl = "/automation-practice-form",
-            headerTitleMainPage = "Student Registration Form",
-            firstName = "Steve",
-            lastName = "Rogers",
-            userEmail = "captainamerica@marvel.com",
-            gender = "Male",
-            userNumber = "8885115480",
-            dateBirthday = "19.07.1918",
-            subject = "Art",
-            hobby = "Sports",
-            pathPicture = "src/test/resources/capitanamerica.jpg",
-            currentAddress = "Earth-616, New York City",
-            state = "NCR",
-            city = "Noida",
-            headerModalTable = "Thanks for submitting the form";
 
     @BeforeAll
     static void openBrowser() {
@@ -41,68 +21,43 @@ public class Les10TestFormJenkins {
     }
 
     @Test
-    void fillTextForm() {
+    void fillTestForm() {
         SelenideLogger.addListener("allure", new AllureSelenide());
-
-        step("Open page " + openPagesUrl, () -> {
-            registrationPage.openPage(openPagesUrl, headerTitleMainPage);
-        });
-        step("Insert First Name " + firstName, () -> {
-            registrationPage.setFirstNameInput(firstName);
-        });
-        step("Insert Last Name " + lastName, () -> {
-            registrationPage.setLastNameInput(lastName);
-        });
-        step("Insert User Email " + userEmail, () -> {
-            registrationPage.setUserEmailInput(userEmail);
-        });
-        step("Check Gender " + gender, () -> {
-            registrationPage.checkGenderRadioButton(gender);
-        });
-        step("Insert User Number " + userNumber, () -> {
-            registrationPage.setUserNumberInput(userNumber);
-        });
-        step("Check Date Birthday " + dateBirthday, () -> {
-            registrationPage.setDateBirthday(dateBirthday);
-        });
-        step("Check Subject " + subject, () -> {
-            registrationPage.selectSubjectInput(subject);
-        });
-        step("Check Hobby " + hobby, () -> {
-            registrationPage.checkBoxHobbies(hobby);
-        });
-        step("Upload Picture " + pathPicture, () -> {
-            registrationPage.setPathUploadPicture(pathPicture);
-        });
-        step("Insert CurrentAddress " + currentAddress, () -> {
-            registrationPage.setCurrentAddress(currentAddress);
-        });
-        step("Insert State " + state, () -> {
-            registrationPage.setStateInput(state);
-        });
-        step("Insert city " + city, () -> {
-            registrationPage.setCityInput(city);
-        });
-        step("Click button", () -> {
-            registrationPage.clickButtonSubmit();
-        });
-        step("Assert data Modal Table", () -> {
-            registrationPage.checkModalTable(headerModalTable);
-            registrationPage.checkForm("Student Name", firstName + " " + lastName)
-                    .checkForm("Student Email", userEmail)
-                    .checkForm("Gender", gender)
-                    .checkForm("Student Email", userEmail)
-                    .checkForm("Mobile", userNumber)
-                    .checkForm("Date of Birth", calendarComponent.converterDate(dateBirthday))
-                    .checkForm("Subjects", subject)
-                    .checkForm("Hobbies", hobby)
-                    .checkForm("Picture", pathComponent.converterCheckPath(pathPicture))
-                    .checkForm("Address", currentAddress)
-                    .checkForm("State and City", state + " " + city);
-        });
-
+        open("/automation-practice-form");
+        $(".practice-form-wrapper > h5").shouldHave(text("Student Registration Form"));
+        $("#firstName").setValue("Steve");
+        $("#lastName").setValue("Rogers");
+        $("#userEmail").setValue("captainamerica@marvel.com");
+        $("#genterWrapper").find(byText("Male")).click();
+        $("#userNumber").setValue("8885115480");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__year-select").selectOptionByValue("1918");
+        $(".react-datepicker__month-select").selectOptionByValue("6");
+        $(".react-datepicker__day--004").click();
+        $("#subjectsInput").setValue("Art").pressEnter();
+        $(byText("Sports")).click();
+        $("#uploadPicture").uploadFile(new File("src/test/resources/capitanamerica.jpg"));
+        $("#currentAddress").setValue("Earth-616, New York City");
+        $("#state").click();
+        $(byText("NCR")).click();
+        $("#city").click();
+        $("#react-select-4-input").setValue("Noi").pressEnter();
+        $(byText("Submit")).click();
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $("table").shouldHave(
+                text("Steve Rogers"),
+                text("captainamerica@marvel.com"),
+                text("Male"),
+                text("8885115480"),
+                text("04 July,1918"),
+                text("Arts"),
+                text("Sports"),
+                text("capitanamerica.jpg"),
+                text("Earth-616, New York City"),
+                text("NCR Noida"));
+        $("#closeLargeModal").shouldHave(text("Close")).click();
     }
-
+    
     @AfterEach
     void addAttachments() {
         AttachAllure.screenshotAs("Last screenshot");
