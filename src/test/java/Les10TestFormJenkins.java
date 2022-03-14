8c79b93aee006696d29c2c75c24f27425ab2fac3
TatianaCheb <tanya.cheba12@gmail.com>
2022-03-14 11:02:17 +0300

    added lesson 11

--- src/test/java/Les10TestFormJenkins.java
+++ src/test/java/Les10TestFormJenkins.java
@@ -1,38 +1,17 @@
-import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.logevents.SelenideLogger;
-import io.qameta.allure.selenide.AllureSelenide;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Configuration.baseUrl;
-import static com.codeborne.selenide.Configuration.browserSize;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
-public class Les10TestFormJenkins {
-
-    @BeforeAll
-    static void openBrowser() {
-        baseUrl = "https://demoqa.com";
-        browserSize = "1920x1080";
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
-
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability("enableVNC", true);
-        capabilities.setCapability("enableVideo", true);
-        Configuration.browserCapabilities = capabilities;
-    }
+public class Les10TestFormJenkins extends TestBase {
 
     @Test
+    @Tag("properties")
     void fillTestForm() {
-
-
         step("Open page ", () -> {
             open("/automation-practice-form");
             $(".practice-form-wrapper > h5").shouldHave(text("Student Registration Form"));
@@ -96,12 +75,9 @@ public class Les10TestFormJenkins {
 
     }
 
-    @AfterEach
-    void addAttachments() {
-        AttachAllure.screenshotAs("Last screenshot");
-        AttachAllure.pageSource();
-        AttachAllure.browserConsoleLogs();
-        AttachAllure.addVideo();
-        closeWebDriver();
+    @Test
+    @Tag("smoke")
+    void smokeTest(){
+        System.out.println(System.getProperty("Hello!!!"));
     }
 }
