b84805e38adb21c0a364b1a25455250dc5fdd532
TatianaCheb <tanya.cheba12@gmail.com>
2022-03-23 09:04:16 +0300

    +

--- src/test/java/Les10TestFormJenkins.java
+++ src/test/java/Les10TestFormJenkins.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class Les10TestFormJenkins extends TestBase {
 
     @Test
-    @Tag("properties")
+//    @Tag("properties")
     void fillTestForm() {
         assertTrue(true);
 //        step("Open page ", () -> {
